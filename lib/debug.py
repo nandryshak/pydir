dc50b59a8818e733e9a5d21e9af9f0b88f6c86fd
Montessquio <Superservercraft@gmail.com>
2018-01-06 15:00:28 -0500

    Add licence

--- lib/debug.py
+++ lib/debug.py
@@ -2,8 +2,29 @@ from time import gmtime
 from sys import exit
 
 '''
-Monty's Debug Library. Copyright Nicolas "Montessquio" Suarez 2018.
+Monty's Debug Library. Please note this software may be licenced differently
+than the software that uses it.
 Designed for use as a python import and should not be executed explicitly.
+
+Copyright (C) 2018 Nicolas "Montessquio" Suarez
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 '''
 
 # Debugger class.
