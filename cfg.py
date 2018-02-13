2f2819ff30030419a63a5a17ed3ea28829c354b7
Montessquio <Superservercraft@gmail.com>
2018-02-13 14:39:01 -0500

    Move for clarity

--- cfg.py
+++ cfg.py
@@ -26,13 +26,12 @@ _FOLLOWSYMLINKS = True      # Should the spider follow symbolic links?
 _ALLOW_OUT_OF_WEBROOT = False # When false, this will restrict symlink destinations to only those within the _WEBROOT
 _WEBROOT = "/var/www/html"   # THIS MUST BE CHANGED FOR SYMLINKS TO WORK.
 
+_SKIPDIRS = False   # Should directories with no changes made since last run be skipped?
+                    # Turn this off if pages aren't all updating properly.
+
 #vvv# Unless you want to debug the program leave this at the default. #vvv#'''
 _LOGLEVEL = 2 # Console debug log level. -1 for none, 0 for info, 1 for warning, 2 for errors, 3 for verbose.
 
 #vvv# These should generally not be modified unless you know what you're doing. #vvv#'''
 _ITEMTEMPLATE = open("rsc/item-template.html").read() # What HTML to duplicate and fill for each file/dir
 _THEME = open("rsc/theme.html").read() # This is the html that should enclose the $content$
-
-
-_SKIPDIRS = False    # Should directories with no changes made since last run be skipped?
-                    # Turn this off if pages aren't all updating properly.
